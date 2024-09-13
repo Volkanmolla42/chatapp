023ca695fa788020c8f7720dcdcdbff0dde4f3b2
Volkan Molla <mollavolkan11@gmail.com>
2024-09-14 01:33:15 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { initializeApp } from 'firebase/app'
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import { getFirestore, collection, query, orderBy, limit, addDoc, serverTimestamp } from 'firebase/firestore'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyB4spfHBeWTOJF_9UA99i2OwW5cp-_K7lw',
+  authDomain: 'firstchatapp-9917f.firebaseapp.com',
+  projectId: 'firstchatapp-9917f',
+  storageBucket: 'firstchatapp-9917f.appspot.com',
+  messagingSenderId: '779142643214',
+  appId: '1:779142643214:web:f95a002d1fa6847db4065f',
+  measurementId: 'G-6NQJRR55ZS',
 }
 
-export default App;
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const firestore = getFirestore(app)
+
+function App() {
+  const [user] = useAuthState(auth)
+
+  return (
+    <div className='App'>
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
+      </header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
+    </div>
+  )
+}
+
+function SignIn() {
+  const signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider()
+    try {
+      await signInWithPopup(auth, provider)
+    } catch (error) {
+      console.error('Error signing in with Google:', error)
+    }
+  }
+
+  return (
+    <>
+      <button className='sign-in' onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && (
+      <button className='sign-out' onClick={() => signOut(auth)}>
+        Sign Out
+      </button>
+    )
+  )
+}
+
+function ChatRoom() {
+  const dummy = useRef()
+  const messagesRef = collection(firestore, 'messages')
+  const q = query(messagesRef, orderBy('createdAt'), limit(25))
+
+  const [messages] = useCollectionData(q, { idField: 'id' })
+
+  const [formValue, setFormValue] = useState('')
+
+  const sendMessage = async (e) => {
+    e.preventDefault()
+    const { uid, photoURL } = auth.currentUser
+
+    try {
+      await addDoc(messagesRef, {
+        text: formValue,
+        createdAt: serverTimestamp(),
+        uid,
+        photoURL,
+      })
+      setFormValue('')
+      dummy.current.scrollIntoView({ behavior: 'smooth' })
+    } catch (error) {
+      console.error('Error sending message:', error)
+    }
+  }
+
+  return (
+    <>
+      <main>
+        {messages && messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+
+        <span ref={dummy}></span>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder='say something nice' />
+
+        <button type='submit' disabled={!formValue}>
+          🕊️
+        </button>
+      </form>
+    </>
+  )
+}
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received'
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} alt='avatar' />
+      <p>{text}</p>
+    </div>
+  )
+}
+
+export default App
